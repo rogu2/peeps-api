89ea701f4c942cb6c4791cc0b42e416c215e2501
rogu2 <rguempel@gmail.com>
2019-07-31 16:25:21 -0400

    Update Auth Routes with middleware
    - Adds middleware for authentication
    - Modifies GET route for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
+const auth = require('../middleware/auth')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
 
 const User = require('../models/User')
-const auth = require('../models/Auth')
 
 // @route   GET    api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-    res.send('Get Logged In User')
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 // @route   POST    api/auth
