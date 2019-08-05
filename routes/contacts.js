eaf63b928a30143104a48a82ff3257ada93ab2b9
rogu2 <rguempel@gmail.com>
2019-08-05 15:25:13 -0400

    Add contact routes
    - Adds Post
    - Adds Put

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const auth = require('../middleware/auth')
+const User = require('../models/User')
+const Contact = require('../models/Contacts')
+
 
 // @route   GET api/contacts
 // @desc    Get all contacts owned by user
 // @access  Private
-router.get('/', (req, res) => {
-    res.send('Gets all contacts')
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 })
+        res.json(contacts)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 // @route   POST api/contacts
 // @desc    Adds new contact to user
 // @access  Private
-router.post('/', (req, res) => {
-    res.send('Add contact')
+router.post('/', [auth, [
+    check('name', 'Name is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name, email, phone, type, birthday } = req.body
+
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            birthday,
+            type,
+            user: req.user.id
+        })
+
+        const contact = await newContact.save()
+        res.json(contact)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 // @route   PATCH api/contacts/:id
