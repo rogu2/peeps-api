1671577d5b91a82f2400194f19eddd2a8ad05284
rogu2 <rguempel@gmail.com>
2019-08-06 10:15:01 -0400

    Add Delete Request to Contact Routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -86,8 +86,22 @@ router.patch('/:id', auth, async (req, res) => {
 // @route   DELETE api/contacts/:id
 // @desc    Deletes contact
 // @access  Private
-router.delete('/:id', (req, res) => {
-    res.send('Update Contact')
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let contact = await Contact.findById(req.params.id)
+        if (!contact) return res.status(404).json({ msg: 'Contact Not Found' })
+
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not Authorized' })
+        }
+
+        await Contact.findByIdAndRemove(req.params.id)
+        
+        res.json({ msg: 'Contact Removed' })
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 module.exports = router
