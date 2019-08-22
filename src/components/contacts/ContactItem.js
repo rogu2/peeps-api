61788c9474fca285296bb4707f8885397177c0ca
rogu2 <rguempel@gmail.com>
2019-08-22 13:10:35 -0400

    Add Delete Contact - Adds delete event listener and calls onDelete method to ContactItem component - Adds onDelete method in Contact State - Adds DELETE_CONTACT case to contact Reducer - Adds Delete Contact to Provider in Contact State

--- src/components/contacts/ContactItem.js
+++ src/components/contacts/ContactItem.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
+import ContactContext from '../../context/contact/contactContext'
 
 const ContactItem = ({ contact }) => {
 
+    const contactContext = useContext(ContactContext)
+    const { deleteContact } = contactContext
+
     const { id, name, email, phone, type, birthday } = contact
+
+    const onDelete = () => {
+        deleteContact(id)
+    }
+
     return (
         <div className='card bg-light'>
             <h3 className='text-primary text-left'>
-                {name}
-                {' '}<span style={{ float: 'right' }}
-                className={'badge ' + (type === 'professional' ?
-                'badge-success' : 'badge-primary')}>
+                {name}{' '}
+                <span style={{ float: 'right' }}
+                    className={
+                        'badge ' + 
+                        (type === 'professional' ? 'badge-success' : 'badge-primary')
+                    }
+                >
                 {type.charAt(0).toUpperCase() + type.slice(1)}
                 </span>
             </h3>
@@ -30,7 +42,7 @@ const ContactItem = ({ contact }) => {
             </ul>
             <p>
                 <button className="btn btn-dark btn-sm">Edit</button>
-                <button className="btn btn-danger btn-sm">Delete</button>
+                <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
             </p>
         </div>
     )
--- src/context/contact/ContactState.js
+++ src/context/contact/ContactState.js
@@ -50,6 +50,9 @@ const ContactState = props => {
         dispatch({ type: ADD_CONTACT, payload: contact })
     }
     // Delete Contact
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id })
+    }
 
     // Set Current Contact
 
@@ -65,7 +68,8 @@ const ContactState = props => {
         <ContactContext.Provider
         value={{
             contacts: state.contacts,
-            addContact
+            addContact,
+            deleteContact
         }}>
             { props.children }
         </ContactContext.Provider>
