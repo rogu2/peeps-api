1cc750b8616b033ac220d465e809c790dffb0e1c
rogu2 <rguempel@gmail.com>
2019-08-23 14:55:22 -0400

    Add redirect after user registration - Adds isAuthenticated method to Register component - Adds useContext and useEffect hooks to Home component for authentication - Adds loaduser method to AuthState

--- src/context/auth/AuthState.js
+++ src/context/auth/AuthState.js
@@ -55,6 +55,7 @@ const AuthState = props => {
                 type: REGISTER_SUCCESS,
                 payload: res.data
             })
+            loadUser()
         } catch (err) {
             dispatch({
                 type: REGISTER_FAIL,
