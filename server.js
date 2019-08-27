6d16b151654bb9c5cdc4e343b8d148b54eec8efd
rogu2 <rguempel@gmail.com>
2019-08-27 16:25:40 -0400

    modifies production env

--- server.js
+++ server.js
@@ -10,18 +10,18 @@ connectDB()
 // Middleware
 app.use(express.json({ extended: false }))
 
+// Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+
  // Serve static assets in PROD
-if (process.env.NODE_ENV === 'production') {
+ if (process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express.static('client/build'))
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
-
-// Define Routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
 
 const PORT = process.env.PORT || 2000
 
