ef1d7e941733f1d71de6f02c242620b2230699f5
rogu2 <rguempel@gmail.com>
2019-08-27 16:10:56 -0400

    modified serverjs

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json({ extended: false }))
  // Serve static assets in PROD
 if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('client/build'))
+    app.use(express.static('client/public'))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html')))
 }
 
 // Define Routes
