0748b1bd286ad9d292e2e18e515fd60abb5701ac
rogu2 <rguempel@gmail.com>
2019-08-27 13:07:18 -0400

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -10,6 +11,14 @@ connectDB()
 app.use(express.json({ extended: false }))
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to Peeps API' }))
+
+// Serve static assets in PROD
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 // Define Routes
 app.use('/api/users', require('./routes/users'))
